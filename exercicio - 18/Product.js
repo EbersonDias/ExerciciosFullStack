46b8c4d8347af1a8de6f48d2ff1a4ceb367751fe
EbersonDias <ebersoncms@gmail.com>
2024-08-03 00:52:37 -0300

    Criando Classes

--- exercicio - 18/Product.js
+++ exercicio - 18/Product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Product{
     constructor(name, description, price){
         this.name = name
@@ -19,26 +18,4 @@ const priceWithDiscount = watch.calculateDiscount(15)
 
 console.log(watch)
 console.log(priceWithDiscount)
-=======
-class Product{
-    constructor(name, description, price){
-        this.name = name
-        this.description = description
-        this.price = price
-        this.inStock = 0
-    }
-    addToStock(quantity){
-        this.inStock += quantity
-    }
-    calculateDiscount(discount){
-        return this.price * ((100 - discount) / 100)
-    }
-}
-const watch = new Product ("Relogio de Pulso","...", 80)
-watch.addToStock(99)
-const priceWithDiscount = watch.calculateDiscount(15)
-
-console.log(watch)
-console.log(priceWithDiscount)
->>>>>>> b8870bcb907e486142cd9bcf2de0f64187e65047
 console.log(watch.calculateDiscount(10))
--- exercicio - 18/User.js
+++ exercicio - 18/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class User {
     constructor(fullname, email, password){
         this.fullname = fullname
@@ -16,23 +15,4 @@ class User {
 
 const john = new User("John Doe", "john@email.com", "1234")
 console.log(john)
-=======
-class User {
-    constructor(fullname, email, password){
-        this.fullname = fullname
-        this.email = email
-        this.password = password
-    }
-    login(email, password){
-        if (this.email === email && this.password === password){
-            console.log("Login Realizado com Sucesso!")
-        }else{
-            console.log("Falha ao fazer Login! Email ou senha incorretos.")
-        }    
-    }
-}
-
-const john = new User("John Doe", "john@email.com", "1234")
-console.log(john)
->>>>>>> b8870bcb907e486142cd9bcf2de0f64187e65047
 john.login("john@email.com", "1234")
