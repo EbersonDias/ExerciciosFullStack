a3f1b4214d89e6ce4c2a8ddf3052f32b8197e23f
EbersonDias <ebersoncms@gmail.com>
2024-08-23 00:08:43 -0300

    Treinando o uso das Promises IMC

--- exercicio - 24/index.js
+++ exercicio - 24/index.js
@@ -1,42 +1,30 @@
 // Função assíncrona que calcula o IMC
-async function calcularIMC(peso, altura) {
+function imc(weight, height) {
     return new Promise((resolve, reject) => {
-        if (typeof peso !== 'number' || typeof altura !== 'number') {
-            reject('Os parâmetros peso e altura devem ser números.');
-        } else {
-            const imc = peso / (altura * altura);
-            resolve(imc);
-        }
-    });
-}
-
+      if (typeof weight  !== 'number' || typeof height !== 'number')
+        reject('arguments must be of type number')
+      else
+        resolve(weight / (height * height))
+    })
+  }
 // Função que chama a função calcularIMC e exibe o resultado
-function exibirIMC(peso, altura) {
-    calcularIMC(peso, altura)
-        .then(imc => {
-            console.log(`IMC: ${imc.toFixed(2)}`);
-            if (imc < 18.5) {
-                console.log('Situação: magreza');
-            } else if (imc >= 18.5 && imc <= 24.9) {
-                console.log('Situação: normal');
-            } else if (imc >= 25 && imc <= 29.9) {
-                console.log('Situação: sobrepeso');
-            } else if (imc >= 30 && imc <= 39.9) {
-                console.log('Situação: obesidade');
-            } else {
-                console.log('Situação: obesidade grave');
-            }
-        })
-        .catch(error => {
-            console.error(`Erro: ${error}`);
-        })
-        .finally(() => {
-            console.log('Cálculo do IMC finalizado.');
-        });
-
-    // Mensagem para evidenciar o funcionamento assíncrono
-    console.log('Aguardando o cálculo do IMC...');
-}
-
+function showImc(weight, height) {
+    imc(weight, height).then((result) => {
+      console.log(`O resultado do IMC foi de ${result}.`)
+  
+      if (result < 18.5) console.log('Situação: MAGREZA')
+      else if (result < 25) console.log('Situação: NORMAL')
+      else if (result < 30) console.log('Situação: SOBREPESO')
+      else if (result < 40) console.log('Situação: OBESIDADE')
+      else console.log('Situação: OBESIDADE GRAVE')
+    }).catch((err) => {
+      console.log(err)
+    })
+    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+  }
 // Exemplo de uso
-exibirIMC(71, 1.64);
+showImc(71, 1.74)
+showImc(48, 1.60)
+showImc(71, "texto")
+showImc(82, 1.72)
+showImc(120, 1.80)
